1ec26f716bb78b61ff941fc2a077427c8961a1f3
Pawel Dampc <dampc86pawel@gmail.com>
2022-05-18 14:05:49 +0200

    Init

---
+++ src/main/java/com/fastkom/FastkomApplication.java
@@ -0,0 +1,14 @@
+package com.fastkom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastkomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastkomApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/fastkom/FastkomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastkom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastkomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
